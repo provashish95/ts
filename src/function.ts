b01b7d2b0d3af2ed9f04d9f64aa23c9e132c5f53
provashish95 <roytonmoy95@gmail.com>
2023-06-05 21:31:23 +0600

    upadted

--- src/function.ts
+++ src/function.ts
@@ -1,50 +1,34 @@
-function add(num1: number, num2: number): number {
+//Normal function 
+// function add(num1, num2) {
+//   return num1 + num2; 
+// }
+
+//Normal Function with type 
+function add(num1: number, num2: number) {
   return num1 + num2;
 }
 
-const addArrow = (num1: number, num2: number): number => num1 + num2;
+//Arrow function with type
+let hello = (n1: number, n2: number): number => n1 * n2;
+// console.log(hello(30, 20));
 
-const arr = [1, 2, 3];
+//Array map with type
+const numbers = [2, 4, 5, 6];
+const newArr = numbers.map((elem: number) => elem * elem);
+// console.log(newArr);
 
-const newArray = arr.map((elem: number) => elem * elem);
 
-
+//Function in a object with type 
 const person: {
   name: string,
   balance: number,
-  addBalance(money: number): string,
+  addBalance(money: number): void
 } = {
-  name: "Tonmoy",
+  name: 'Roy',
   balance: 5,
   addBalance(money: number) {
-    return `My new balance is : ${this.balance + money}`;
-  },
-
+    console.log(`My new balance is ${this.balance + money}`);
+  }
 }
 
-//Default parameter not set at first parameter..
-
-function defaultParameter(num1: number, num2: number = 10): number {
-  return num1 + num2;
-}
-defaultParameter(30);
-
-//spread operator in ts
-const myFriends = ['chayan', 'shipan', 'foysal'];
-const newFriends = ['kamal', 'jamal', 'damal'];
-
-myFriends.push(...newFriends);
-console.log(myFriends);
-
-//Rest operator in ts
-const greetFriends = (...friends: string[]): void => friends.forEach((friend) => console.log(`Hi ${friend}`));
-
-greetFriends('Provashish', 'Tonmoy', 'Roy', 'rakin', 'maruf', 'Pritam');
-
-//Array and Object destructuring 
-const student = {
-  fullName: 'Athoi sarker',
-  age: 20
-}
-const { fullName: myName } = student;
-//console.log(myName);
+console.log(person.addBalance(200));
